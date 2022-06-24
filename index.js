dec8356cbd235f76c8d7af64e8ea017d4eee7c81
Daeyeon Cho <107440788+daecho8@users.noreply.github.com>
2022-06-24 14:56:35 -0500

    part 1 finished

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
+// function start() {
+//   var myCounter = 8;
+// }
+// start();
+
+// Calling function immediately; combined version
+// (function () {
+//   var myCounter = 8;
+// }) ()
+
 (function() {
   'use strict';
 
   // YOUR CODE HERE
+  let stopButton = document.getElementById('stopButton');
+  let stopLight = document.getElementById('stopLight');
+
+  stopButton.addEventListener('click', () => {
+    stopLight.classList.toggle('stop');
+  });
+  
+
+  let slowButton = document.getElementById('slowButton');
+  let slowLight = document.getElementById('slowLight');
+
+  slowButton.addEventListener('click', () => {
+    slowLight.classList.toggle('slow');
+  });
+
+  let goButton = document.getElementById('goButton');
+  let goLight = document.getElementById('goLight');
+
+  goButton.addEventListener('click', () => {
+    goLight.classList.toggle('go');
+});
+
 })();
+
