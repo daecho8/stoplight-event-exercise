03f08847a5b489351d118209241074569b8d3e82
Daeyeon Cho <107440788+daecho8@users.noreply.github.com>
2022-06-24 17:57:44 -0500

    Bonus finished

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// function start() {
-//   var myCounter = 8;
-// }
-// start();
-
-// Calling function immediately; combined version
-// (function () {
-//   var myCounter = 8;
-// }) ()
-
 // PART 1
 // (function() {
 //   'use strict';
@@ -39,16 +29,16 @@
 
 
 // //PART 2
-(function() {
-  'use strict';
+// (function() {
+//   'use strict';
 
-  // YOUR CODE HERE
-let stopButton = document.getElementById('stopButton');
-let stopLight = document.getElementById('stopLight');
-let slowButton = document.getElementById('slowButton');
-let slowLight = document.getElementById('slowLight');
-let goButton = document.getElementById('goButton');
-let goLight = document.getElementById('goLight');
+//   // YOUR CODE HERE
+// let stopButton = document.getElementById('stopButton');
+// let stopLight = document.getElementById('stopLight');
+// let slowButton = document.getElementById('slowButton');
+// let slowLight = document.getElementById('slowLight');
+// let goButton = document.getElementById('goButton');
+// let goLight = document.getElementById('goLight');
 
 // 1st way
 // stopButton.addEventListener('mouseenter', () => {
@@ -71,16 +61,66 @@ let goLight = document.getElementById('goLight');
 // });
 
 // 2nd way
-[stopButton, slowButton, goButton].forEach((button) => {
-  button.addEventListener('mouseenter', (event) => {
-    console.log(`Enter ${event.target.innerText} button`)   
+// [stopButton, slowButton, goButton].forEach((button) => {
+//   button.addEventListener('mouseenter', (event) => {
+//     console.log(`Enter ${event.target.innerText} button`)   
+//   });
+
+//   button.addEventListener('mouseleave', (event) => {
+//     console.log(`Left ${event.target.innerText} button`)   
+//   });
+// });
+
+// })();
+
+//Bonus
+(function() {
+  'use strict';
+  let stopButton = document.getElementById('stopButton');
+  let stopLight = document.getElementById('stopLight');
+  let slowButton = document.getElementById('slowButton');
+  let slowLight = document.getElementById('slowLight');
+  let goButton = document.getElementById('goButton');
+  let goLight = document.getElementById('goLight');
+  
+  let countStop = 0;
+  stopButton.addEventListener('click', (e) => {
+    if (e.target && e.target.matches('.button')) {
+      stopLight.classList.toggle('stop');
+      
+      if (countStop % 2 === 0) {
+        console.log(`${e.target.innerText} bulb on`);
+      } else {
+        console.log(`${e.target.innerText} bulb off`); 
+      }
+    } countStop++;
+  });
+  
+  let countSlow = 0;
+  slowButton.addEventListener('click', (e) => {
+    if (e.target && e.target.matches('.button')) {
+      slowLight.classList.toggle('slow');
+      
+      if (countSlow % 2 === 0) {
+        console.log(`${e.target.innerText} bulb on`);
+      } else {
+        console.log(`${e.target.innerText} bulb off`); 
+      }
+    } countSlow++;
   });
 
-  button.addEventListener('mouseleave', (event) => {
-    console.log(`Left ${event.target.innerText} button`)   
+  let countGo = 0;
+  goButton.addEventListener('click', (e) => {
+    if (e.target && e.target.matches('.button')) {
+      goLight.classList.toggle('go');
+      
+      if (countGo % 2 === 0) {
+        console.log(`${e.target.innerText} bulb on`);
+      } else {
+        console.log(`${e.target.innerText} bulb off`); 
+      }
+    } countGo++;
   });
-});
+
 
 })();
-
-
